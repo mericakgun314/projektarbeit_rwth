11b9abcbd20c0eed56862777b96cd4036a03ad23
Meric Akgün <mericakgun314@gmail.com>
2021-11-23 18:51:00 +0300

    Loops the CT-Scans

--- porosity_data.py
+++ porosity_data.py
@@ -1,4 +1,22 @@
 from PIL import Image
 import os
 
-path = "C:/Users/User/Desktop/CT Scans Final/1/1 _1"
+# Path of the folder, which contains the CT-Scans of all the samples 
+path = "C:/Users/User/Desktop/CT Scans Final/"
+
+# Loops the samples
+for sample in os.listdir(path):
+
+    # Loops the 15 elements of each sample
+    for element in os.listdir(path + sample):
+
+        # Loops the 3 surfaces of each element
+        for scan in os.listdir(path + sample + "/" + element):
+
+            # Ignores the files not containing the relevant scans
+            if "Ebene" in scan:
+
+                # Loops the scans of each surface
+                for surface in os.listdir(path + sample + "/" + element + "/" + scan):
+                    print(surface)
+                
