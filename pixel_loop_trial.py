ea5224fdd1d2953674ef51c3ff57b6f2eb607be9
Meric Akgün <mericakgun314@gmail.com>
2021-11-30 19:27:24 +0300

    Tries to speed up by using numpy

--- pixel_loop_trial.py
+++ pixel_loop_trial.py
@@ -8,5 +8,14 @@ path = "C:/Users/User/Desktop/CT Scans Final/1/1 1/Ebene 1/1 1 Ebene 116.jpg"
 # img_pixel = image.load()
 
 img = numpy.asarray(Image.open(path))
+img.transpose(2,0,1).reshape(3,-1)
+a = 0
+b = 0
+red_array =numpy.array([254, 0, 0])
 
-print(img[1].size)
+for i in img:
+    for j in i:
+        if (j==red_array).all():
+            b= b + 1
+
+print(b)
